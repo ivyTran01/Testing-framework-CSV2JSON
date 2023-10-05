c39aa1a6a0cd090189006f0d9737d240b1b7631c
Ivy <vynguyenaitran@gmail.com>
2023-10-05 14:37:15 -0700

    generate all test cases.

--- generateTestFiles.py
+++ generateTestFiles.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import copy
 
 TEST_FILE_PATH_PREFIX = "TestData/TestFiles/test"
 TEST_FILE_PATH_SUFFIX = ".csv"
@@ -22,49 +23,89 @@ def getTestFilePath(index):
 
 
 def generateTest(specs):
-    # file size
-    # file header
-    #
-    # value_empty_string
-    # value_null
-    #
-    # value_contains_special_character
-    # value_contains_quotes
-    #
-    # value_leading_whitespace
-    # value_trailing_whitespace
-    #
-    # value_quoted (should be handled auto when "value contains special char")
-    # value_contains_escape_double_quotes (should be handled auto when "value contains quotes")
-    #
-    #
-    # file delimiter //set with csv writer
-    # file_space_after_delimiter //set with csv writer
-
     global data
-    if specs['file_size'] == 1:
-        if specs['file_header']:
-            data = [DATA_TEMPLATE[0]]
-        else:
-            data = [DATA_TEMPLATE[1]]
-    elif specs['file_size'] == 2:
-        if specs['file_header']:
-            data = DATA_TEMPLATE[:]
-        else:
-            data = DATA_TEMPLATE[1:]
+    headerFlag = specs['file_header']
+    file_size = specs['file_size']
 
-    print(data)
-    return data
+    if specs['file_delimiter'] == "comma":
+        file_delimiter = ','
+    elif specs['file_delimiter'] == "semicolon":
+        file_delimiter = ';'
+    else:
+        file_delimiter = '\t'
+    # ---------------------------------------------
+    if specs['value_data_type'] == "string":
+        column = 0
+    elif specs['value_data_type'] == "number":
+        column = 1
+    else:
+        column = 2
+    # ---------------------------------------------
+    if specs['file_size'] == 1:
+        if headerFlag:
+            data = copy.deepcopy([DATA_TEMPLATE[0]])
+        else:
+            data = copy.deepcopy([DATA_TEMPLATE[1]])
+    elif specs['file_size'] == 2:
+        if headerFlag:
+            data = copy.deepcopy(DATA_TEMPLATE[:])
+        else:
+            data = copy.deepcopy(DATA_TEMPLATE[1:])
+    # -------------------------------------
+    if specs['value_empty_string']:
+        if specs['file_size'] == 1:
+            data[0][column] = '""'
+            return data, csv.QUOTE_NONE, file_delimiter
+        else:
+            data[1][column] = '""'
+            return data, csv.QUOTE_NONE, file_delimiter
+
+    if specs['value_null']:
+        if specs['file_size'] == 1:
+            data[0][column] = 'null'
+            return data, csv.QUOTE_MINIMAL, file_delimiter
+        else:
+            data[1][column] = 'null'
+            return data, csv.QUOTE_MINIMAL, file_delimiter
+    # ----------------------------------------
+    if specs['value_contains_special_character']:
+        if file_size == 1:
+            data[0][column] = data[0][column] + file_delimiter
+        else:
+            data[1][column] = data[1][column] + file_delimiter
+    # ---------------------------------------
+    if specs['value_contains_quotes']:
+        if file_size == 1:
+            data[0][column] = data[0][column] + " \"inside\" end"
+        else:
+            data[1][column] = data[1][column] + " \"inside\" end"
+    # ---------------------------------------
+    if specs['value_leading_whitespace']:
+        if file_size == 1:
+            data[0][column] = " " + data[0][column]
+        else:
+            data[1][column] = " " + data[1][column]
+    # ---------------------------------------
+    if specs['value_trailing_whitespace']:
+        if file_size == 1:
+            data[0][column] = data[0][column] + " "
+        else:
+            data[1][column] = data[1][column] + " "
+
+    return data, csv.QUOTE_MINIMAL, file_delimiter
 
 
 def main():
     testFrames = getTestFramesDF()
 
     for index, row in testFrames.iterrows():
-        testData = generateTest(row)
+        testData, quotingOption, delimiterOption = generateTest(row)
         testPath = getTestFilePath(index)
         with open(testPath, mode="w", newline="") as file:
-            csv_writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+            if quotingOption == csv.QUOTE_MINIMAL:
+                csv_writer = csv.writer(file, quoting=quotingOption, delimiter=delimiterOption)
+            else:
+                csv_writer = csv.writer(file, quoting=quotingOption, quotechar=' ', delimiter=delimiterOption)
             for record in testData:
                 csv_writer.writerow(record)
 
